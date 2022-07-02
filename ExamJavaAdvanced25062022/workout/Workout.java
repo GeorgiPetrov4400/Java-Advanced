55f7dbd19da4ebe97b2b3cc0a564f6f2ef7a8b8c
Georgi Petrov <102458157+GeorgiPetrov4400@users.noreply.github.com>
2022-07-02 08:37:37 +0300

    Add files via upload

---
+++ ExamJavaAdvanced25062022/workout/Workout.java
@@ -0,0 +1,81 @@
+package ExamJavaAdvanced25062022.workout_03;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Workout {
+    private String type;
+    private int exerciseCount;
+    private List<Exercise> exercises;
+
+    public Workout(String type, int exerciseCount) {
+        this.type = type;
+        this.exerciseCount = exerciseCount;
+        this.exercises = new ArrayList<>();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getExerciseCount() {
+        return exerciseCount;
+    }
+
+    public void setExerciseCount(int exerciseCount) {
+        this.exerciseCount = exerciseCount;
+    }
+
+    public List<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(List<Exercise> exercises) {
+        this.exercises = exercises;
+    }
+
+    public void addExercise(Exercise exercise) {
+        if (exercises.size() < exerciseCount) {
+            exercises.add(exercise);
+        }
+    }
+
+    public boolean removeExercise(String name, String muscle) {
+        for (Exercise exercise : exercises) {
+            if (exercise.getName().equals(name) && exercise.getMuscle().equals(muscle)) {
+                exercises.remove(exercise);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Exercise getExercise(String name, String muscle) {
+        for (Exercise exercise : exercises) {
+            if (exercise.getName().equals(name) && exercise.getMuscle().equals(muscle)) {
+                return exercise;
+            }
+        }
+        return null;
+    }
+
+    public Exercise getMostBurnedCaloriesExercise() {
+        return this.exercises.stream().max(Comparator.comparing(Exercise::getBurnedCalories)).orElse(null);
+    }
+
+    public String getStatistics() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("Workout type: %s", this.type)).append("\n");
+        for (Exercise exercise : exercises) {
+            builder.append(exercise.toString());
+            builder.append("\n");
+        }
+        return builder.toString().trim();
+    }
+}
+
