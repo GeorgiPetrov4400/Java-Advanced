46819db4b93e2579ba40790602c2eae82ae8d186
Georgi Petrov <102458157+GeorgiPetrov4400@users.noreply.github.com>
2022-07-02 08:40:10 +0300

    Add files via upload

---
+++ ExamPreparation062022/groomingSalon/GroomingSalon.java
@@ -0,0 +1,68 @@
+package ExamPreparation062022.groomingSalon_03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroomingSalon {
+    private int capacity;
+    private List<Pet> data;
+
+    public GroomingSalon(int capacity) {
+        this.capacity = capacity;
+        this.data = new ArrayList<>();
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<Pet> getData() {
+        return data;
+    }
+
+    public void setData(List<Pet> data) {
+        this.data = data;
+    }
+
+    public void add(Pet pet) {
+        if (data.size() < capacity) {
+            data.add(pet);
+        }
+    }
+
+    public boolean remove(String name) {
+        for (Pet pet : data) {
+            if (pet.getName().equals(name)) {
+                data.remove(pet);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Pet getPet(String name, String owner) {
+        for (Pet pet : data) {
+            if (pet.getName().equals(name) && pet.getOwner().equals(owner)) {
+                return pet;
+            }
+        }
+        return null;
+    }
+
+    public int getCount() {
+        return data.size();
+    }
+
+    public String getStatistics() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("The grooming salon has the following clients:").append("\n");
+        for (Pet pet : data) {
+            builder.append(pet.getName()).append(" ").append(pet.getOwner()).append("\n");
+        }
+        return builder.toString();
+    }
+}
