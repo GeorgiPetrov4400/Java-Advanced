c9b7912af87ee8a554eee045371b68c0e1ab8bb2
Georgi Petrov <102458157+GeorgiPetrov4400@users.noreply.github.com>
2022-07-02 08:40:48 +0300

    Add files via upload

---
+++ ExamPreparation062022/guild88/Player.java
@@ -0,0 +1,60 @@
+package ExamPreparation062022.guild88_03;
+
+public class Player {
+    private String name;
+    private String clazz;
+    private String rank;
+    private String description;
+
+    public Player(String name, String clazz) {
+        this.name = name;
+        this.clazz = clazz;
+        this.rank = "Trial";
+        this.description = "n/a";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClazz() {
+        return clazz;
+    }
+
+    public void setClazz(String clazz) {
+        this.clazz = clazz;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        String nameAndClazz = String.format("Player %s: %s", this.name, this.clazz);
+        String rank = String.format("Rank: %s", this.rank);
+        String description = String.format("Description: %s", this.description);
+        builder.append(nameAndClazz).append("\n");
+        builder.append(rank).append("\n");
+        builder.append(description);
+
+        return builder.toString();
+    }
+}
