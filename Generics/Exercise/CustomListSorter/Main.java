5a9e2bced9e588de81584e2cd7cc6b1e7bdfb2f2
Georgi Petrov <102458157+GeorgiPetrov4400@users.noreply.github.com>
2022-07-05 07:30:25 +0300

    Add files via upload

---
+++ Generics/Exercise/CustomListSorter/Main.java
@@ -0,0 +1,58 @@
+package GenericsExercise.CustomListSorter;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        CustomList<String> customList = new CustomList<>();
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("END")) {
+            String[] data = input.split("\\s+");
+            String command = data[0];
+            switch (command) {
+                case "Add":
+                    String elementToAdd = data[1];
+                    customList.add(elementToAdd);
+                    break;
+                case "Remove":
+                    int index = Integer.parseInt(data[1]);
+                    customList.remove(index);
+                    break;
+                case "Contains":
+                    String elementToMatch = data[1];
+                    System.out.println(customList.contains(elementToMatch));
+                    break;
+                case "Swap":
+                    int firstIndex = Integer.parseInt(data[1]);
+                    int secondIndex = Integer.parseInt(data[2]);
+                    customList.swap(firstIndex, secondIndex);
+                    break;
+                case "Greater":
+                    String elementToCheck = data[1];
+                    System.out.println(customList.countGreaterElement(elementToCheck));
+                    break;
+                case "Max":
+                    System.out.println(customList.max());
+                    break;
+                case "Min":
+                    System.out.println(customList.min());
+                    break;
+                case "Print":
+                    System.out.println(customList.toString());
+                    break;
+                case "Sort":
+                    Sorter.sort(customList);
+                    break;
+                default:
+                    System.out.println("Invalid command");
+                    break;
+            }
+
+            input = scanner.nextLine();
+        }
+    }
+}
