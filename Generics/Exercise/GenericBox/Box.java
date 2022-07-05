7e34dba55c7f4c8223198c24b4008c42690071e4
Georgi Petrov <102458157+GeorgiPetrov4400@users.noreply.github.com>
2022-07-05 07:31:03 +0300

    Add files via upload

---
+++ Generics/Exercise/GenericBox/Box.java
@@ -0,0 +1,24 @@
+package GenericsExercise.GenericBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box <T> {
+    private List<T> value;
+
+    public Box() {
+        this.value = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        this.value.add(element);
+    }
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (T element : value) {
+            builder.append(String.format("%s: %s%n", element.getClass().getName(), element.toString()));
+        }
+        return builder.toString();
+    }
+}
