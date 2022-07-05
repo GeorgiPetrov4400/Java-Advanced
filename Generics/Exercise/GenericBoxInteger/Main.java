6110340ff9b185abe033ebdec7bcde5a34097eaa
Georgi Petrov <102458157+GeorgiPetrov4400@users.noreply.github.com>
2022-07-05 07:31:42 +0300

    Add files via upload

---
+++ Generics/Exercise/GenericBoxInteger/Main.java
@@ -0,0 +1,19 @@
+package GenericsExercise.GenericBoxInteger;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Box<Integer> box = new Box<>();
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            int input = Integer.parseInt(scanner.nextLine());
+            box.add(input);
+        }
+        System.out.println(box);
+    }
+}
