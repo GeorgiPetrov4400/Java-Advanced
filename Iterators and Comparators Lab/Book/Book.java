3565fcd0e0b85be0c3e4ba6a7962d1cfabdbc38f
Georgi Petrov <102458157+GeorgiPetrov4400@users.noreply.github.com>
2022-07-06 10:58:23 +0300

    Add files via upload

---
+++ Iterators and Comparators Lab/Book/Book.java
@@ -0,0 +1,39 @@
+package IteratorsAndComparatorsLab.Book;
+
+import java.util.List;
+
+public class Book {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        setTitle(title);
+        setYear(year);
+        setAuthors();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String... authors) {
+        this.authors = List.of(authors);
+    }
+}
